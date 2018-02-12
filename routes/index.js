634392527ade13c39ab66c56bb8aa0348e659359
Coronel <c_coronel@tss-hr420-dm19.fc.ca>
2018-02-12 12:33:54 -0500

    adding stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  { name: "Jade", usefulness : "sucky" },
+  { name: "EJS", usefulness : "a little better" },
+  { name: "Hardlebars", usefulness : "awesome" }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message : "I really hate Jade" });
+  res.render('index', {
+    title: 'Express',
+    message : "I like EJS",
+    templates : templateTypes
+  });
 });
 
 module.exports = router;
